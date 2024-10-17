8fdac73157a66ba804696593945ac445cf8f997d
SAndresArboleda <simondres@yahoo.es>
2024-10-17 14:33:07 -0500

    inicio git

---
+++ Front/src/views/Home/Home.jsx
@@ -0,0 +1,11 @@
+import { NavPpal } from "../../Componentes/navPpal"
+
+export const Home =()=>{
+    return(
+        <div>
+            <header>
+               <NavPpal/>
+            </header>
+        </div>
+    )
+}
